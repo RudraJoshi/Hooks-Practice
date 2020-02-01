db4f1244f7561b0d3a8e56aeda5303c6ccdce78e
RudraJoshi <rudrajoshi1604@gmail.com>
2020-02-01 17:06:55 +0530

    Second Commit

--- src/Components/Chapter08/CounterEight.js
+++ src/Components/Chapter08/CounterEight.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import React,{useReducer} from 'react'
+
+const initialState = 0
+
+const reducer = (state , action) => {
+
+    if(action.type === 'INC'){
+        return state + 1
+    }
+    if(action.type === 'DEC'){
+        return state - 1
+    }
+    if(action.type === 'RESET'){
+        return initialState;
+    }
+    
+    return state;
+}
 
 function CounterEight() {
+
+  const [count,dispatch] =   useReducer(reducer,initialState)
+
     return (
         <div>
             <h2>Chapter 08 [Reducer] </h2>
-                
+           
+    <h3>Count - {count}</h3>
+                <div>
+                    <button onClick={() => dispatch({type:'INC'})}>Increament</button>
+                    <button onClick={() => dispatch({type:'DEC'})}>Decrement</button>
+                    <button onClick={() => dispatch({type:'RESET'})}>Reset</button>
+                </div>
+
 
             <hr/>
         </div>
